338fec0b6e3ee78220a2a6927e57c2c287234af1
Paul McCloud <paul.mccloud@gmail.com>
2017-05-23 12:48:14 +0200

    use path.join

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static(path(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 8080)
