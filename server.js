f0df5189b70b2728e66f1e00eeb752ca45cc03be
Paul McCloud <paul.mccloud@gmail.com>
2017-05-23 11:48:43 +0200

    server loggin

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
+    console.log('Index path is: ', indexPath)
     const publicPath = express.static(path.join(__dirname, 'public'))
+    console.log('Public path is: ', publicPath)
 
     app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
