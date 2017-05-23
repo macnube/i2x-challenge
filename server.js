4481db7436e341db97ac229608750bf20bf8825c
Paul McCloud <paul.mccloud@gmail.com>
2017-05-23 12:06:07 +0200

    fixed public path

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/public'))
 
 app.listen(process.env.PORT || 8080)
