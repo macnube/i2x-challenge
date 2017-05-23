a77fecb6dedb65698800aa14cb9391cfa4eb3770
Paul McCloud <paul.mccloud@gmail.com>
2017-05-23 12:01:59 +0200

    cleaned up server config

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const path = require('path')
 const express = require('express')
+const app = express()
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, '/../index.html')
-    console.log('Index path is: ', indexPath)
-    const publicPath = express.static(path.join(__dirname, 'public'))
-    console.log('Public path is: ', publicPath)
+app.use(express.static(__dirname + '/'))
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
-
-    return app
-  }
-}
+app.listen(process.env.PORT || 8080)
